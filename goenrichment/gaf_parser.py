b88af59bad4b4a879932ce3dc5d4b6390f317b96
Pieter <13552343+pmoris@users.noreply.github.com>
2017-02-20 14:40:36 +0100

    Add import function for gaf file without specifiying background set

--- goenrichment/gaf_parser.py
+++ goenrichment/gaf_parser.py
@@ -25,13 +25,13 @@ def importGAF(path, geneSet):
     Returns
     -------
     dict of str mapping to set
-        A dictionary mapping gene uniprot AC's to GO ID's.
+        A dictionary mapping gene uniprot AC's (str) to a set GO ID's.
 
 
     Possible improvements:
         Check for `is_obsolete` and `replaced_by`, although the replacement term should be in OBO file as an entry.
 
-        Check for inclusion in provided geneset afterwards using:
+        Check for inclusion in provided gene set afterwards using:
             gafDict = {key: value for key, value in gafDict.items() if key in geneSet }
 
         To do: double dictionary? already filter based on gene list? what to do with NOT?
@@ -59,13 +59,54 @@ def importGAF(path, geneSet):
                         else:                               # set of GO's as value
                             gafDict[uniprotAC].add(goTerm)
 
-    print('Retrieved', len(gafDict),
-          'annotated (background filtered) uniprot AC\'s from', gafPath + '\n')
-
     if len(gafDict) != len(geneSet):
         print('WARNING!\nNot every uniprot AC that was provided in the background set was found in the GAF file:')
         print([AC for AC in geneSet if AC not in gafDict])
         print('WARNING!\n')
+
+    print('Retrieved', len(gafDict),
+          'annotated (background filtered) uniprot AC\'s from', gafPath + '\n')
+
+    return gafDict
+
+def importFullGAF(path):
+    """
+    Imports a GAF file (gene association format) and generates a dictionary mapping the gene uniprot AC to the GO ID.
+    Imports the full set of genes in the file.
+
+    Information on the GAF 2.1 format can be found at
+        http://geneontology.org/page/go-annotation-file-gaf-format-21
+
+    Parameters
+    ----------
+    path : str
+        The path to the file.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping gene uniprot AC's (str) to a set of GO ID's.
+   """
+
+    gafPath = os.path.abspath(path)
+
+    with open(gafPath, 'r') as gafFile:
+        gafDict = {}
+        for line in gafFile:
+            if line.startswith('UniProtKB'):
+                splitLine = line.split('\t')                # split column-wise
+                uniprotAC = splitLine[1]
+                goTerm = splitLine[4]
+                goQualifier = splitLine[3]
+                if 'NOT' not in goQualifier:                # ignore annotations with "NOT"
+                    if uniprotAC not in gafDict:        # Create new key if AC does not already appear in dictionary
+                        # make dictionary containing uniprot AC as key and
+                        gafDict[uniprotAC] = {goTerm}
+                    else:                               # set of GO's as value
+                        gafDict[uniprotAC].add(goTerm)
+
+    print('Retrieved', len(gafDict),
+          'annotated uniprot AC\'s from', gafPath, 'after filtering on the background set.' + '\n')
 
     return gafDict
 
