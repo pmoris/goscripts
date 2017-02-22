8c8f40faec3b6cbb305bc4d130dc22275b4d5120
Pieter <13552343+pmoris@users.noreply.github.com>
2017-02-22 16:10:54 +0100

    Correct python shebang

--- data-preprocessing/entrezTaxIDlookup.py
+++ data-preprocessing/entrezTaxIDlookup.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # http://biopython.org/DIST/docs/tutorial/Tutorial.html
 
 import sys,csv, time
 from Bio import Entrez
+
 Entrez.email = 'pieter.moris@uantwerpen.be'
-
-
 
 with open(sys.argv[1],'r') as f:
     taxIDlist = [line.rstrip() for line in f]
 
-with open("taxonomyID.csv",'w') as o:
+with open(sys.argv[2],'w') as o:
     writer = csv.writer(o,lineterminator='\n',delimiter='\t')
     writer.writerow(['Taxonomy ID','Scientific Name'])
 
@@ -19,6 +18,4 @@ with open("taxonomyID.csv",'w') as o:
         time.sleep(0.5)
         handle = Entrez.efetch(db="Taxonomy", id = id)
         records = Entrez.read(handle)
-#        o.write(id+'\t'+records[0]["ScientificName"].strip()+'\n')
-#        writer.writerow(id + '\t' + records[0]["ScientificName"].strip())
-        writer.writerow([id,records[0]['ScientificName'].rstrip()])
+        writer.writerow([id, records[0]['ScientificName'].rstrip()])
